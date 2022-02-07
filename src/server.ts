b01cd56e5018d8d1e1d93f0b8e845f28e4f0d832
Geraldo Ahnert <geraldoahnert@gmail.com>
2022-02-07 10:14:03 -0300

    feat: creating category

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,12 @@
 import express from 'express';
+import { categoriesRoutes } from './routes/categories.routes';
 
 const app = express();
 const port = 3333;
 
 app.use(express.json());
 
-app.post('/courses', (request, response) => {
-  const { name } = request.body;
-  return response.json({ name });
-});
+app.use(categoriesRoutes)
 
 app.listen(port, () => {
   console.log(`Server is running at ${port}.`);
