1b8b4b95dd5fb01b79e9ada48d64ad3c2a1a61d9
Geraldo Ahnert <geraldoahnert@gmail.com>
2022-02-08 12:40:05 -0300

    feat: create category repository

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,33 @@
+import { Category } from '../model/Category'
+
+// DTO -> Data Transfer Object
+interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+}
+
+class CategoriesRepository {
+  private categories: Category[];
+
+  constructor(){
+    this.categories = [];
+  }
+
+  create({ description, name }: ICreateCategoryDTO): void{
+    const category = new Category();
+
+    // category.name = name;
+    // category.description = description;
+    // category.created_at = new Date();
+    // * Refac to Object Assign
+    Object.assign(category, {
+      name,
+      description,
+      created_at: new Date()
+    })
+  
+    this.categories.push(category)
+  }
+}
+
+export { CategoriesRepository }
