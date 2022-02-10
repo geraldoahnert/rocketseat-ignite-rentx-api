3a90b1612ea6f23fc0bc1113f3e13180bd125e01
Geraldo Ahnert <geraldoahnert@gmail.com>
2022-02-10 18:51:50 -0300

    fix: imports

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,7 +1,6 @@
 import { Router } from 'express';
-import { CategoriesRepository } from '../repositories/CategoriesRepository';
-import { Category } from '../model/Category';
-import { CreateCategoryService } from '../services/CreateCategoryService';
+import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
+import { CreateCategoryService } from '../modules/cars/services/CreateCategoryService';
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
