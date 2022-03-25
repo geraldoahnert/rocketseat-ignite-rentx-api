4b5c8bb630e03258860a7ad1b8b3cf31c2d56482
Geraldo Ahnert <geraldoahnert@gmail.com>
2022-03-25 11:24:47 -0300

    feat: get sub from verify jwt and verify if valid

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,5 +1,10 @@
 import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
+import { UsersRepository } from '../modules/accounts/repositories/implementations/UsersRepository';
+
+interface IPayload {
+  sub: string;
+}
 
 export async function ensureAuthenticated(
   request: Request,
@@ -17,8 +22,16 @@ export async function ensureAuthenticated(
   const [, token] = authHeader.split(' ');
 
   try {
-    const decoded = verify(token, 'batatasecreta');
-    console.log(decoded);
+    const { sub: user_id } = verify(token, 'batatasecreta') as IPayload;
+
+    const usersRepository = new UsersRepository();
+    const user = usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error('User does not exists!');
+    }
+
+    next();
   } catch {
     throw new Error('Invalid token!');
   }
