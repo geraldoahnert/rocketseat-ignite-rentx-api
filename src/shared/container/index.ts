6ae2be3faaee04da1d84c556879de3260f91fa5a
Geraldo Ahnert <geraldoahnert@gmail.com>
2022-05-23 12:43:46 -0300

    fix id routes and create the container for upload car images

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,29 +1,36 @@
-import { container } from 'tsyringe';
+import { ICarsImagesRepository } from "@modules/cars/infra/typeorm/repositories/ICarsImagesRepository";
+import { container } from "tsyringe";
 
-import { ICategoriesRepository } from '@modules/cars/repositories/ICategoriesRepository';
-import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
+import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository";
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 
-import { CategoriesRepository } from '@modules/cars/infra/typeorm/repositories/CategoriesRepository';
-import { SpecificationsRepository } from '@modules/cars/infra/typeorm/repositories/SpecificationsRepository';
-import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
-import { CarsRepository } from '@modules/cars/infra/typeorm/repositories/CarsRepository';
-import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+import { CategoriesRepository } from "@modules/cars/infra/typeorm/repositories/CategoriesRepository";
+import { SpecificationsRepository } from "@modules/cars/infra/typeorm/repositories/SpecificationsRepository";
+import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
+import { CarsRepository } from "@modules/cars/infra/typeorm/repositories/CarsRepository";
+import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 
-import { ISpecificationsRepository } from '@modules/cars/repositories/ISpecificationInMemory';
+import { ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationInMemory";
+import { CarsImagesRepository } from "@modules/cars/infra/typeorm/repositories/CarsImagesRepository";
 
 container.registerSingleton<ICategoriesRepository>(
-  'CategoriesRepository',
+  "CategoriesRepository",
   CategoriesRepository
 );
 
 container.registerSingleton<ISpecificationsRepository>(
-  'SpecificationsRepository',
+  "SpecificationsRepository",
   SpecificationsRepository
 );
 
 container.registerSingleton<IUsersRepository>(
-  'UsersRepository',
+  "UsersRepository",
   UsersRepository
 );
 
-container.registerSingleton<ICarsRepository>('CarsRepository', CarsRepository);
+container.registerSingleton<ICarsRepository>("CarsRepository", CarsRepository);
+
+container.registerSingleton<ICarsImagesRepository>(
+  "CarsImagesRepository",
+  CarsImagesRepository
+);
