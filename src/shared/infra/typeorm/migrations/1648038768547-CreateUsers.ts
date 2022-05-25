e2e38997b7d4180092a78d155429458b069c514c
Geraldo Ahnert <geraldoahnert@gmail.com>
2022-05-25 19:21:42 -0300

    add users id is primary true

--- src/shared/infra/typeorm/migrations/1648038768547-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1648038768547-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1648038768547 implements MigrationInterface {
           {
             name: 'id',
             type: 'uuid',
+            isPrimary: true,
           },
           {
             name: 'name',
