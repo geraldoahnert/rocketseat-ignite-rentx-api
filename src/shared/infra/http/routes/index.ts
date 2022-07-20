acc40043e2b7eff9cea018ffe6c8edecb697865f
Geraldo Ahnert <geraldoahnert@gmail.com>
2022-07-20 20:44:23 -0300

    creating the email provider

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,21 +1,24 @@
-import { Router } from "express";
+import { Router } from 'express';
 
-import { authenticateRoutes } from "./authenticate.routes";
-import { categoriesRoutes } from "./categories.routes";
-import { specificationsRoutes } from "./specifications.routes";
-import { usersRouters } from "./users.routes";
-import { carsRoutes } from "./cars.routes";
-import { rentalRoutes } from "./rental.routes";
+import { authenticateRoutes } from './authenticate.routes';
+import { categoriesRoutes } from './categories.routes';
+import { specificationsRoutes } from './specifications.routes';
+import { usersRouters } from './users.routes';
+import { carsRoutes } from './cars.routes';
+import { rentalRoutes } from './rental.routes';
+import { passwordRoutes } from './password.routes';
 
 const router = Router();
 
-router.use("/categories", categoriesRoutes);
-router.use("/specifications", specificationsRoutes);
+router.use('/categories', categoriesRoutes);
+router.use('/specifications', specificationsRoutes);
 
-router.use("/users", usersRouters);
+router.use('/users', usersRouters);
 
-router.use("/cars", carsRoutes);
-router.use("/rentals", rentalRoutes);
+router.use('/cars', carsRoutes);
+router.use('/rentals', rentalRoutes);
+
+router.use('/password', passwordRoutes);
 
 router.use(authenticateRoutes);
 
